69d54d366f8deb9791b45601daf000ba6eb50dd2
Gobukgol <zxczc159@gmail.com>
2018-10-30 21:22:34 +0900

    fix ReviewActivity to ReviewFragment

---
+++ app/src/main/java/dmzing/workd/view/review/ReviewFragment.kt
@@ -0,0 +1,19 @@
+package dmzing.workd.view.review
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dmzing.workd.R
+
+class ReviewFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater!!.inflate(R.layout.fragment_review,container)
+
+        return view
+    }
+
+
+}
