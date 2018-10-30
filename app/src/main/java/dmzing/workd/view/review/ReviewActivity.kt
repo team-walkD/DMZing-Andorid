d3ce8697afbcbedbf2990c3c61e32b722aa244f7
Gobukgol <zxczc159@gmail.com>
2018-10-30 19:45:18 +0900

    fix font name and add review activity

---
+++ app/src/main/java/dmzing/workd/view/review/ReviewActivity.kt
@@ -0,0 +1,13 @@
+package dmzing.workd.view.review
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import dmzing.workd.R
+
+class ReviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_review)
+    }
+}
