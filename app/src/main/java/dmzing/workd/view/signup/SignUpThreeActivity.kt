cc2ead9c80ab632d72082a6bce4768a43c7fdc67
Woo <jhsw0375@gmail.com>
2018-11-01 16:44:54 +0900

    sign up activity 3단계 구현

---
+++ app/src/main/java/dmzing/workd/view/signup/SignUpThreeActivity.kt
@@ -0,0 +1,13 @@
+package dmzing.workd.view.signup
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import dmzing.workd.R
+
+class SignUpThreeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_three)
+    }
+}
