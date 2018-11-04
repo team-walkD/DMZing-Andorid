9210bda1138e514f8b1c9856fdf7d9126af8e6f7
Gobukgol <zxczc159@gmail.com>
2018-11-04 15:36:47 +0900

    review main page incomplete

---
+++ app/src/main/java/dmzing/workd/view/review/ReviewMapViewHolder.kt
@@ -0,0 +1,14 @@
+package dmzing.workd.view.review
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import de.hdodenhof.circleimageview.CircleImageView
+import dmzing.workd.R
+
+class ReviewMapViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+    var mapImage : CircleImageView = itemView.findViewById(R.id.review_map_image)
+    var mapText : TextView = itemView.findViewById(R.id.review_map_title)
+    var mapCount : TextView = itemView.findViewById(R.id.review_map_count)
+}
