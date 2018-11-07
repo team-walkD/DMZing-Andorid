dcfc734737b2fc85ba43ac9ec5cbfbc8723ac39c
Woo <jhsw0375@gmail.com>
2018-11-07 19:21:33 +0900

    activity 수정

---
+++ app/src/main/java/dmzing/workd/view/course/CourseDetailActivity.kt
@@ -0,0 +1,13 @@
+package dmzing.workd.view.course
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import dmzing.workd.R
+
+class CourseDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_course_detail)
+    }
+}
