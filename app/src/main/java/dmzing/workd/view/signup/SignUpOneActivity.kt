b242d04d674e4654e68ed309120d252c30f42615
Woo <jhsw0375@gmail.com>
2018-11-20 16:07:52 +0900

    login 화면, 회원 가입 3단계 화면 뷰 수정 작업

--- app/src/main/java/dmzing/workd/view/signup/SignUpOneActivity.kt
+++ app/src/main/java/dmzing/workd/view/signup/SignUpOneActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import dmzing.workd.R
 import dmzing.workd.util.Utils
 import kotlinx.android.synthetic.main.activity_sign_up_one.*
+import org.jetbrains.anko.toast
 
 class SignUpOneActivity : AppCompatActivity(), View.OnClickListener, Utils {
 
@@ -17,7 +18,22 @@ class SignUpOneActivity : AppCompatActivity(), View.OnClickListener, Utils {
 
     override fun onClick(v: View?) {
         when(v!!){
-            signUpOneNextBtn-> startActivity(Intent(this, SignUpTwoActivity::class.java))
+            signUpOneNextBtn->{
+                if(signUpOneName.text.length > 0 && signUpOneNickname.text.length > 0){
+                    var name : String = signUpOneName.text.toString()
+                    var nickname : String = signUpOneNickname.text.toString()
+                    var intent = Intent(this@SignUpOneActivity, SignUpTwoActivity::class.java)
+                    intent.putExtra("name",name)
+                    intent.putExtra("nickname",nickname)
+                    startActivity(intent)
+                }else{
+                    toast("정보를 입력해주세요.")
+
+                }
+
+            }
+
+
         }
     }
 
