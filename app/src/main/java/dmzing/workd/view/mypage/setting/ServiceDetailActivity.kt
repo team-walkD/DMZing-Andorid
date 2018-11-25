50452335e746cfa63a2ab97e8a087dcef2417adf
Woo <jhsw0375@gmail.com>
2018-11-25 16:13:49 +0900

    개인정보 취급 방침 수정

--- app/src/main/java/dmzing/workd/view/mypage/setting/ServiceDetailActivity.kt
+++ app/src/main/java/dmzing/workd/view/mypage/setting/ServiceDetailActivity.kt
@@ -6,6 +6,9 @@ import android.view.View
 import dmzing.workd.R
 import kotlinx.android.synthetic.main.activity_service_detail.*
 import org.jetbrains.anko.toast
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.io.InputStream
 
 class ServiceDetailActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
@@ -28,6 +31,46 @@ class ServiceDetailActivity : AppCompatActivity(), View.OnClickListener {
     private fun getData() {
         var data = intent.getStringExtra(ServiceActivity.DETAIL_TITLE)
         serviceDetailTitle.text = data.toString()
+        settingText(data)
+    }
+
+    fun settingText(text: String) {
+        when(text){
+            "DMZing 안내"->{
+                serviceText.setText(readText(resources.openRawResource(R.raw.privacy)))
+            }
+            "이용 약관"->{
+                serviceText.setText(readText(resources.openRawResource(R.raw.privacy)))
+            }
+            "개인정보 보호 정책"->{
+                serviceText.setText(readText(resources.openRawResource(R.raw.privacy)))
+            }
+
+        }
+
+    }
+
+    fun readText(file : InputStream): String? {
+        var data: String? = null
+
+        val inputStream = file
+        val byteArrayOutputStream = ByteArrayOutputStream()
+
+        var i: Int
+        try {
+            i = inputStream.read()
+            while (i != -1) {
+                byteArrayOutputStream.write(i)
+                i = inputStream.read()
+            }
+
+            data = String(byteArrayOutputStream.toByteArray())
+            inputStream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return data
     }
 
     private fun init() {
